413074052b8b612dbef5d70c38563d674aae3818
KenTang-9Purple <tangtszyik@gmail.com>
2024-11-27 15:34:48 +0800

    3rd

--- src/components/IconLink.jsx
+++ src/components/IconLink.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function IconLink({link,itemClass}) {
   return (
     <li>
-        <a href={link.href} className={itemClass}>
+        <a href={link.href} className={itemClass} target='_blank'>
             <i className={link.icon}></i>
         </a>
     </li>
